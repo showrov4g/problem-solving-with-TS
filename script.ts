a3e2d20bdfdb3ad0116bf9d74a68c22ae0633526
showrov4g <showrovg00@gmail.com>
2025-05-07 23:45:28 +0600

    problem solving

--- script.ts
+++ script.ts
@@ -9,5 +9,7 @@ function formatString(input: string, toUpper?: boolean): string{
 
 
 function filterByRating(items: { title: string; rating: number }[]): { title: string; rating: number }[] {
-    
+    return items.filter((item)=>item.rating >=4);
 }
+
+
