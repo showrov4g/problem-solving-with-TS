3c05b2c6f79ef7f95801443b78b37097eb309a5e
showrov4g <showrovg00@gmail.com>
2025-05-08 00:22:32 +0600

    problem number 3 solve

--- script.ts
+++ script.ts
@@ -14,7 +14,11 @@ function filterByRating(items: { title: string; rating: number }[]): { title: st
 
 
 function concatenateArrays<T>(...arrays: T[][]): T[]{
-    
+    let output: T[] = [];
+    for(let item of arrays){
+        output = output.concat(item)
+    }
+    return output;
 }
-
-
+console.log(concatenateArrays(["a", "b"], ["c"]))
+console.log(concatenateArrays([1, 2], [3, 4], [5]))
