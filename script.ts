075026b70ba5053b67e86647f8d30fe432f733f2
showrov4g <showrovg00@gmail.com>
2025-05-08 07:39:18 +0600

    try to solve problem 4

--- script.ts
+++ script.ts
@@ -1,7 +1,7 @@
-function formatString(input: string, toUpper?: boolean): string{
-    if(toUpper === false){
-        return  input.toLocaleLowerCase();
-    }else{
+function formatString(input: string, toUpper?: boolean): string {
+    if (toUpper === false) {
+        return input.toLocaleLowerCase();
+    } else {
         return input.toLocaleUpperCase();
     }
 
@@ -9,16 +9,29 @@ function formatString(input: string, toUpper?: boolean): string{
 
 
 function filterByRating(items: { title: string; rating: number }[]): { title: string; rating: number }[] {
-    return items.filter((item)=>item.rating >=4);
+    return items.filter((item) => item.rating >= 4);
 }
 
 
-function concatenateArrays<T>(...arrays: T[][]): T[]{
+function concatenateArrays<T>(...arrays: T[][]): T[] {
     let output: T[] = [];
-    for(let item of arrays){
+    for (let item of arrays) {
         output = output.concat(item)
     }
     return output;
 }
-console.log(concatenateArrays(["a", "b"], ["c"]))
-console.log(concatenateArrays([1, 2], [3, 4], [5]))
+
+class Vehicle {
+    private make: string;
+    private year: number;
+    constructor(make: string, year: number) {
+        this.make = make;
+        this.year = year;
+    }
+    public getInfo():string{
+        return `Make ${this.make} Year:${this.year}`
+    }
+    
+
+
+}
