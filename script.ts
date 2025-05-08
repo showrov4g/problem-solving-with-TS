ee8c0d352ab69928a37ba87a333452e8e40a543c
showrov4g <showrovg00@gmail.com>
2025-05-08 07:53:46 +0600

    solve probelm 4

--- script.ts
+++ script.ts
@@ -29,16 +29,22 @@ class Vehicle {
         this.year = year;
     }
     public getInfo(): string {
-        return `Make ${this.make} Year:${this.year}`
+        return `Make: ${this.make} Year:${this.year}`
     }
 
 }
 
-class car extends Vehicle {
+class Car extends Vehicle {
     private model:string;
     constructor(make:string, year:number, model: string){
         super(make,year);
         this.model = model;
     }
-    
+    public getModel():string{
+        return (`Model: ${this.model}`)
+    }
 }
+const myCar = new Car("Toyota", 2020, "Corolla");
+
+console.log(myCar.getInfo());
+console.log(myCar.getModel());
